5069ba8b460ccf284d5049273e79bdcd6aa3acac
aswinmohanme <dev.aswinmohan@gmail.com>
2020-07-16 10:43:22 +0530

    Add Testing Phase

--- main.py
+++ main.py
@@ -23,32 +23,32 @@ def _load_emoticons(emotions):
     """
     return [nparray_as_image(cv2.imread('graphics/%s.png' % emotion, -1), mode=None) for emotion in emotions]
 
+def plot_model_history(model_history):
+    """
+    Plot Accuracy and Loss curves given the model_history
+    """
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
+    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    fig.savefig('plot.png')
+    plt.show()
 
 def emotion(mode):
     # plots accuracy and loss curves
-    def plot_model_history(model_history):
-        """
-        Plot Accuracy and Loss curves given the model_history
-        """
-        fig, axs = plt.subplots(1,2,figsize=(15,5))
-        # summarize history for accuracy
-        axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
-        axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
-        axs[0].set_title('Model Accuracy')
-        axs[0].set_ylabel('Accuracy')
-        axs[0].set_xlabel('Epoch')
-        axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
-        axs[0].legend(['train', 'val'], loc='best')
-        # summarize history for loss
-        axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-        axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
-        axs[1].set_title('Model Loss')
-        axs[1].set_ylabel('Loss')
-        axs[1].set_xlabel('Epoch')
-        axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-        axs[1].legend(['train', 'val'], loc='best')
-        fig.savefig('plot.png')
-        plt.show()
 
 # Define data generators
     train_dir = 'data/train'
@@ -178,7 +178,7 @@ def createGUI():
     startRunningLabel.config(font=(default_font, 12))
     startRunningLabel.place(x=16, y=210)
 
-    testWithData = tkinter.Button(root, text="Test with Data")
+    testWithData = tkinter.Button(root, text="Test with Data", command=testWithDataEvent)
     testWithData.place(x=16, y=240)
 
     openWebcam = tkinter.Button(root, text="Open Webcam", width=40, command=openWebCamEvent) 
@@ -191,6 +191,9 @@ def openWebCamEvent():
     emotion("display")
 
 def trainEvent():
+    emotion("train")
+
+def testWithDataEvent():
     emotion("train")
 
 def main():
